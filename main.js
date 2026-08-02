2b892d4491d58137471b002a82760383182e758d
Nadazh92 <nadalizaher92@gmail.com>
2026-08-02 22:51:44 +0200

    create product

--- main.js
+++ main.js
@@ -6,11 +6,7 @@ const stockQnty=document.getElementById("StockQnty");
 const imgUrl=document.getElementById("imgUrl");
 const descrp=document.getElementById("description");
 const status=document.getElementById("status");
-
-
-
-
-
+const btn=document.getElementById("btnPro");
 
 
 id.onkeyup= function(){
@@ -22,5 +18,39 @@ id.onkeyup= function(){
         id.style.border=' 1px solid #726f6f';
     }  
 }
+
+ let allProducts;
+if (localStorage.product!= null){
+    allProducts = JSON.parse(localStorage.product);
+
+}
+else {
+allProducts=[];
+}
+
+
+btn.onclick=function()
+{
+    
+    let newProduct = {
+    proNumber: id.value,
+    proName: proName.value,
+    catgry: catogery.value,
+    pric: price.value,
+    stokQnty: stockQnty.value,
+    imgUrl: imgUrl.value,
+    descrption: descrp.value,
+    status: status.value
+}
+allProducts.push(newProduct);
+localStorage.setItem('product', JSON.stringify(allProducts));
+// This is way is not enough because when we make reload, the data will be deleted. therefor we must use local storage
+console.log(allProducts);
+
+
+}
+
+
+
 
 
